a0cd9820ec415a8fa5536b1e2568c9b0a32fbe15
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-09-30 00:42:25 +0100

    Add files via upload

--- bord3d.py
+++ bord3d.py
@@ -1,4 +1,20 @@
 print("\033c\033[43;30m\nboard\n")
+def saves(files,arrays):
+    f1=open(files,"w")
+    for n in arrays:
+        f1.write(";")
+        
+        
+        for nn in n:
+             l=False
+             for nnn in nn:
+                 if l:
+                     f1.write(", "+str(nnn))
+                 else:
+                     f1.write(str(nnn))
+                 l=True
+             f1.write("\n")
+    f1.close()
 
 def board3d(x,y,z):
     b=[]
@@ -15,4 +31,5 @@ def board3d(x,y,z):
 
 
 a=board3d(2,2,2)
+saves("my.xyz",a)
 print(a)
